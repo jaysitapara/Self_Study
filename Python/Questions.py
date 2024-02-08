b2e5113660355ad49c4ddbad929ec8fe0a112cb3
Jayu <jaysitapara5103@gmail.com>
2024-02-08 22:39:56 +0530

    done

---
+++ Python/Questions.py
@@ -0,0 +1,27 @@
+# write a proram to input 2 numbera & print their sum
+# num1 = float(input("Enter number 1 : "))
+# num2 = float(input("Enter number 2 : "))
+# print("sum : ", num1 + num2)
+
+# wap to input side of square & print its area
+# side = float(input("Enter square side size : "))
+# print("Side of square area is : ", side * side)
+
+# wap to input 2 floating point numbers & print their average
+# num1 = float(input("Enter number 1 : "))
+# num2 = float(input("Enter number 2 : "))
+# print("Average : ", (num1 + num2) / 2)
+
+# wap to input 2 int number a and b, print true if a us greater than or equal to b. if not print false
+# a = int(input("Enter number 1 : "))
+# b = int(input("Enter number 2 : "))
+# print(a>=b)
+
+# wap to input users first name & print its name
+# name = input("Enter your name :")
+# print(len(name))
+
+# wap to find the occurence of '$' in a string.
+# val = "today $ price is 82.00"
+# print(val.find("$"))
+
---
+++ Python/index.py
@@ -0,0 +1,185 @@
+# sinle line comment
+'''
+multi line
+comment
+'''
+
+# print simle message
+# print("hello world")
+
+# print numbers
+# print(9)
+
+# print directly arithmatic operatins
+# print(5+4)
+
+# variable in python
+# name = "jayu"
+# age = 21
+# price = 21.21
+# print("my name is" ,name, "my age is" ,age, "my item price is" ,price)
+
+# add new Value into old variable
+# name  = "jayu"
+# name = "sitapara"
+# print(name)
+
+# show data type
+# name = "jayu"
+# print(type(name))
+# age = 21
+# print(type(age))
+# price = 21.21
+# print(type(price))
+
+# data type in python
+# a = 10
+# print(type(a))
+# b = "10"
+# print(type(b))
+# c = 10.10
+# print(type(c))
+# d = True
+# print(type(d))
+# e = None
+# print(type(e))
+
+# print sum of two numbers
+# num1 = 2
+# num2 = 5
+
+# sum = num1 + num2
+# print(sum)
+
+# operators on python
+
+# operator in python
+# 1. Arithmatic  operators
+# a = 5
+# b = 2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# 2. Relational / comparison operators
+# a = 5
+# b = 2
+# print(a == b)
+# print(a != b) 
+# print(a < b) 
+# print(a > b) 
+# print(a <= b)
+# print(a >= b) 
+
+# 3. Assignment operators
+# a = 5
+# b = 2
+# a += 10
+# a -= 10
+# a *= 10
+# a /= 10
+# a %= 10
+# a **= 10
+# print (a)
+
+# 4. Logical operators
+# a = 5
+# b = 2
+# val1 = True
+# val2 = True
+# print(not True)
+# print(not False)
+# print(not (a > b))
+# print(val1 and val2)
+# print(val1 or val2)
+
+# type conversion
+# a,b = 1,2.0
+# print(a+b)
+
+# type casting
+# a, b = 1, '2'
+# c = int(b)
+# c = float(b)
+# c = str(b)
+# print(a + c)
+
+# # input from user
+# a = input("Enter your name : ")
+# print(a)
+
+# conver input in int
+# a = int(input("Enter your age : "))
+# print(a)
+
+# conver input in int
+# a = float(input("Enter your age : "))
+# print(a)
+
+
+# string in python
+# str1 = 'Jay Sitapara'
+# str2 = "Jay Sitapara"
+# str3 = '''Jay Sitapara'''
+# print(str1)
+# print(str2)
+# print(str3)
+
+# print one sentence go to into next line
+# str1 = "this is a string. \n we are creating it in python"
+# print(str1)
+
+# tab
+# str1 = "this is a string. \t we are creating it in python"
+# print(str1)
+
+# basic operation in python
+
+# 1. concatenation
+# str1 = "my name is "
+# str2 = "jay"
+# print(str1 + str2)
+
+# 2. string length
+# str = "my name is jay"
+# print(len(str))
+
+# slicing in python
+# str = "jay sitapara"
+# print(str[1:5])
+# print(str[:5])
+# print(str[1:])
+# print(str[1:len(str)])
+
+# slicing in negative index (-1...-2...-3)
+# str = "apple"
+# print(str[-3:-1])
+
+# indexing in python
+# str = "Jay Sitapara"
+# print(str[1])
+
+# string functions
+# str = "my name is jay sitapara"
+
+# finding end substring
+# print(str.endswith("ra"))
+
+# 1st letter capital
+# print(str.capitalize()) # only on this function
+# new = str.capitalize()
+# print(new)  # permenent change
+
+# replace function
+# print(str.replace("jay", "jayu"))
+
+# finding string
+# print(str.find("is"))
+
+# count how many time use word
+# print(str.count("j"))
+
+# conditional statements
