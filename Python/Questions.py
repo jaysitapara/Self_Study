7ddbcdc62149ec817a38b10ba40eb5f6ce1e4aac
Jayu <jaysitapara5103@gmail.com>
2024-02-09 20:53:06 +0530

    dn

--- Python/Questions.py
+++ Python/Questions.py
@@ -2,29 +2,29 @@
 # num1 = float(input("Enter number 1 : "))
 # num2 = float(input("Enter number 2 : "))
 # print("sum : ", num1 + num2)
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to input side of square & print its area
 # side = float(input("Enter square side size : "))
 # print("Side of square area is : ", side * side)
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to input 2 floating point numbers & print their average
 # num1 = float(input("Enter number 1 : "))
 # num2 = float(input("Enter number 2 : "))
 # print("Average : ", (num1 + num2) / 2)
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to input 2 int number a and b, print true if a us greater than or equal to b. if not print false
 # a = int(input("Enter number 1 : "))
 # b = int(input("Enter number 2 : "))
 # print(a>=b)
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to input users first name & print its name
 # name = input("Enter your name :")
 # print(len(name))
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to find the occurence of '$' in a string.
 # val = "today $ price is 82.00"
 # print(val.find("$"))
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to print students grade based on marks
 # marks = float(input("Enter your marks : "))
 # if (marks >= 90):
@@ -35,22 +35,29 @@
 #     print("Grade C")
 # else:
 #     print("Grade D")
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to check if a number entered by the user is odd or even
 # num = int(input("Enter number to check number id odd or even : "))
 # if(num % 2 == 0):
 #     print("Even")
 # else:
 #     print("Odd")
-
+# -----------------------------------------------------------------------------------------------------------
 # wap to find the greatest of 3 numbers enterd by the user
-a = int(input("Enter number a : "))
-b = int(input("Enter number b : "))
-c = int(input("Enter number c : "))
-if(a>=b and a>=c):
-    print("Your greater number is A : ",a)
-elif(b>=a and b>=c):
-    print("Your greater number is B : ",b)
-else:
-    print("Your greater number is C : ",c)
-
+# a = int(input("Enter number a : "))
+# b = int(input("Enter number b : "))
+# c = int(input("Enter number c : "))
+# if(a>=b and a>=c):
+#     print("Your greater number is A : ",a)
+# elif(b>=a and b>=c):
+#     print("Your greater number is B : ",b)
+# else:
+#     print("Your greater number is C : ",c)
+# -----------------------------------------------------------------------------------------------------------
+# wap to check if a number is a multiple of 7 or not
+# number = int(input("Enter number to check multiple is 7 or not : "))
+# if(number % 7 == 0):
+#     print("Yes, number id multiple of 7")
+# else:
+#     print("No, Not multiple by 7")
+# -----------------------------------------------------------------------------------------------------------
