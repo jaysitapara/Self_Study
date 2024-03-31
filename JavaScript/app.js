4c6ed11caeb2b4e466080eea9f6efdc3c0aeb57b
Jayu <jaysitapara5103@gmail.com>
2024-03-31 11:18:28 +0530

    update axios

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1631,7 +1631,7 @@
 //   try {
 //     let res = await fetch(url);
 //     let data = await res.json();
-//     console.log(data.fact);
+//     console.log(data.fact); 
 //   } catch (error) {
 //     console.log("error", error);
 //   }
