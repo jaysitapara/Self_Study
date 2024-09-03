ac24dda9ffa0987d72f0e32b238fda5b0fea8f10
Jay Sitapara <jaysitapara5103@gmail.com>
2024-09-03 22:58:27 +0530

    Day 1 & 2 of DSA

---
+++ lecture_2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // // datatype
+    // int a = 10;
+    // char grade = 'A';
+    // float pi = 3.14F;
+    // bool isSafe = true;
+    // double price = 100.99;
+
+    // // type conversion
+    // char a1 = 'A';
+    // int a2 = a1;
+
+    // // type casting
+    // double oldPrice = 100.99;
+    // int newPrice = (int)oldPrice;
+
+    // cout << a << endl << sizeof(a) << endl;
+    // cout << grade << endl;
+    // cout << pi << endl;
+    // cout << isSafe << endl;
+    // cout << price << endl;
+    // cout << a2 << endl;
+    // cout << newPrice << endl;
+
+    // // input from user
+    // int num;
+    // cout << "Enter number : ";
+    // cin >> num;
+    // cout << "Your number is : " << num << endl;
+
+    // //operators
+    // // Arithmatic ooperator
+    // int a = 5, b = 10;
+    // cout << "- : " << a + b << endl;
+    // cout << "- : " << a - b << endl;
+    // cout << "* : " << a * b << endl;
+    // cout << "/ : " << a / b << endl;
+    // cout << "% : " << a % b << endl;
+
+    // // Relational operator
+    // int a1 = 10, b1 = 20;
+    // cout << "< : " << (a1 < b1) << endl;
+    // cout << "> : " << (a1 > b1) << endl;
+    // cout << "<= : " << (a1 <= b1) << endl;
+    // cout << ">= : " << (a1 >= b1) << endl;
+    // cout << "== : " << (a1 == b1) << endl;
+    // cout << "!= : " << (a1 != b1) << endl;
+
+    // logical operator
+    // int a2 = 100, b2 = 200, c2 = 30, d2 = 400;
+    // cout << "AND : " << ((a2 < b2) && (c2 > d2)) << endl;
+    // cout << "OR : " << ((a2 < b2) || (c2 > d2)) << endl;
+    // cout << "NOT : " << !(a2 < b2) << endl;
+
+    // Unary operator
+    // int a3 = 1, b3 = 1;
+    // cout << "Increment ++ : " << ++a3 << endl;
+    // cout << "decrement -- : " << b3-- << endl;
+    return 0;
+}
